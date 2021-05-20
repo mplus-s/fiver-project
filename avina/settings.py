5402dd27642d742af9528742329840223214e1c1
Husnain Ahmad <husnainahmad521@gmail.com>
2021-05-20 22:20:30 +0500

    database setting changed

--- avina/settings.py
+++ avina/settings.py
@@ -90,12 +90,8 @@ WSGI_APPLICATION = 'avina.wsgi.application'
 DATABASES = {
 
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "AVINA_DATABASE",
-        'USER': "postgres" ,
-        'PASSWORD':"admin" ,
-        'HOST': "localhost",
-        'PORT': "5432",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }    
 }
 
