4bccf2d662f25bc7260fdabff595bfacc8b1eb7b
Husnain Ahmad <husnainahmad521@gmail.com>
2021-05-24 14:16:17 +0500

    products name and price shown in cartitem

--- avina/settings.py
+++ avina/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
 
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'NAME': 'sqlite3.db',
     }    
 }
 
--- products/serializers.py
+++ products/serializers.py
@@ -21,12 +21,26 @@ class ProductSerializer(serializers.ModelSerializer):
         exclude = ( "added_at",)
 
 class CartitemSerializer(serializers.ModelSerializer):
+
+                
+    product_name = serializers.SerializerMethodField('get_productName')
+    product_price = serializers.SerializerMethodField('get_productPrice') 
+    product_image = serializers.SerializerMethodField('get_productImage')
     class Meta:
         model = CartItem
         fields = '__all__'
-
+    def get_productName(self,CartItem):
+        product_name = CartItem.product.name
+        return product_name
+    def get_productPrice(self,CartItem):
+        product_price = CartItem.product.price
+        return product_price 
+    def get_productImage(self,CartItem):
+        product_image = CartItem.product.photo1
+        return product_image               
     def validate(self, validated_data):
         if validated_data["seller_confirmation"] == True & validated_data["buyer_confirmation"] == True:
             validated_data["status"] = "Sold"
             return validated_data
         return validated_data
+
